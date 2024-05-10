47d36b43132dba2686c678d2b9ba101c47171329
eunkyung <ekyi0421@gmail.com>
2024-05-10 17:09:56 +0900

    feat: typography 구조 세팅

---
+++ typography/src/main/java/com/ununique/typography/UqdsTypography.kt
@@ -0,0 +1,14 @@
+package com.ununique.typography
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+private val pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+)
+
+class UqdsTypography {
+
+}
